36379bf1cfe043c134d752194f2ae43897b66d70
Eva-Renata <all2evi@yahoo.com>
2022-09-06 13:18:03 +0200

    footer

--- src/components/Partials/Footer/Footer.jsx
+++ src/components/Partials/Footer/Footer.jsx
@@ -1,7 +1,27 @@
+import styles from "./Footer.module.scss";
+import {
+  AiFillFacebook,
+  AiFillInstagram,
+  AiFillLinkedin,
+} from "react-icons/ai";
+
 export const Footer = () => {
   return (
-    <footer>
-      <h2>This is footer</h2>
+    <footer className={styles.footerwrapper}>
+      <h3>HomeLands</h3>
+      <div>
+        <p>Øster Uttrupvej 5</p>
+        <p>9000 Aalborg</p>
+      </div>
+      <div>
+        <p>Email: info@homelands.dk</p>
+        <p>Telefon: +45 11223344</p>
+      </div>
+      <div className={styles.social}>
+        <AiFillFacebook />
+        <AiFillInstagram />
+        <AiFillLinkedin />
+      </div>
     </footer>
   );
 };
