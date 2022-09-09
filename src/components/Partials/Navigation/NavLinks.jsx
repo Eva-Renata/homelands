dcc4c51d0677d26d2a981e9003dccb112c1ee66c
Eva-Renata <all2evi@yahoo.com>
2022-09-09 11:14:16 +0200

    mediaqueries

--- src/components/Partials/Navigation/NavLinks.jsx
+++ src/components/Partials/Navigation/NavLinks.jsx
@@ -1,19 +1,25 @@
 import { NavLink } from "react-router-dom";
 import styles from "./Navigation.module.scss";
+import { Twirl as Hamburger } from "hamburger-react";
 
 //making navkinks so we can reuse in both mobile and desktop
 export const NavLinks = () => {
   return (
-    <ul className={styles.navlinks}>
-      <li>
-        <NavLink to="/">Forside</NavLink>
-      </li>
-      <li>
-        <NavLink to="/boliger">Boliger til salg</NavLink>
-      </li>
-      <li>
-        <NavLink to="/login">Login</NavLink>
-      </li>
-    </ul>
+    <>
+      <ul className={styles.navlinks}>
+        <li>
+          <NavLink to="/">Forside</NavLink>
+        </li>
+        <li>
+          <NavLink to="/boliger">Boliger til salg</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Login</NavLink>
+        </li>
+      </ul>
+      <div className={styles.hambrgerwrapper}>
+        <Hamburger color="white" className={styles.hamburger} />
+      </div>
+    </>
   );
 };
