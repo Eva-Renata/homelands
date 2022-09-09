1b605310a6c4c0897b8616c10a4091a5ad779829
Eva-Renata <all2evi@yahoo.com>
2022-09-09 12:13:14 +0200

    navlinks mediaq mobil

--- src/components/Partials/Navigation/NavLinks.jsx
+++ src/components/Partials/Navigation/NavLinks.jsx
@@ -1,9 +1,11 @@
 import { NavLink } from "react-router-dom";
 import styles from "./Navigation.module.scss";
 import { Twirl as Hamburger } from "hamburger-react";
+import { useState } from "react";
 
 //making navkinks so we can reuse in both mobile and desktop
 export const NavLinks = () => {
+  const [open, setOpen] = useState(false);
   return (
     <>
       <ul className={styles.navlinks}>
@@ -17,8 +19,26 @@ export const NavLinks = () => {
           <NavLink to="/login">Login</NavLink>
         </li>
       </ul>
-      <div className={styles.hambrgerwrapper}>
+      <div
+        className={styles.hambrgerwrapper}
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
         <Hamburger color="white" className={styles.hamburger} />
+        {open && (
+          <ul className={styles.navlinksopen}>
+            <li>
+              <NavLink to="/">Forside</NavLink>
+            </li>
+            <li>
+              <NavLink to="/boliger">Boliger til salg</NavLink>
+            </li>
+            <li>
+              <NavLink to="/login">Login</NavLink>
+            </li>
+          </ul>
+        )}
       </div>
     </>
   );
